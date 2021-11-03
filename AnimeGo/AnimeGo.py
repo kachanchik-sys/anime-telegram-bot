53ad0016e47d04a0b33404c3f507e290b6bea3e2
kachanchik-sys <72665151+kachanchik-sys@users.noreply.github.com>
2021-11-04 01:30:53 +0300

    Update AnimeGo.py

--- AnimeGo/AnimeGo.py
+++ AnimeGo/AnimeGo.py
@@ -1,16 +1,14 @@
+import json
 from dataclasses import dataclass
 from os import name
 from typing import List, Tuple
-import json
 from urllib.parse import urljoin
 
 import requests
 from lxml import etree
-import json
 
-from requests.api import request
+from webview_module import Response, requests_js
 
-from modules.webview_module import Response, requests_js
 
 class AnimeGO_Exception(Exception):
     pass
@@ -22,6 +20,7 @@ class Anime_finds:
     poster: str
     title: str
 
+
 @dataclass
 class Dub:
     dubber: str
@@ -29,14 +28,13 @@ class Dub:
 
 
 @dataclass
-class Anime:
+class Anime_info:
     poster: str
     title: str
     description: str
-    genre: Tuple[str,...]
+    genre: Tuple[str, ...]
     type: str
     numberOfEpisodes: int
-    dubs: Tuple[Dub, ...]
 
 
 class AnimeGo_Parser():
@@ -71,27 +69,93 @@ class AnimeGo_Parser():
 
         return [(lambda i: Anime_finds(anime_links[i], anime_posters[i], anime_titels_eng[i]))(i) for i in range(len(anime_links))]
 
-    
-    def get_anime_info(self, link: str) -> Anime:
-        xpath_query = '//script[@type="application/ld+json"]'
-        response:Response = requests_js().get_html(link, xpath_query)
-        root: etree._Element = etree.fromstring(
-            response.document,
+    def get_anime_page(self, link) -> None:
+        xpath_query = '//select[@name="series"]'
+        self.response: Response = requests_js().get_html(link, xpath_query)
+        self.root: etree._Element = etree.fromstring(
+            self.response.document,
             etree.HTMLParser(encoding="utf-8")
         )
-        info:dict = json.loads(root.xpath(xpath_query)[0].text)
 
-        anime_info = Anime(
-            title = info.get('name'),
-            description = info.get('description'),
-            type = info.get('@type'),
-            episods = info.get('numberOfEpisodes'),
-            poster = urljoin(self.domain, info.get('image'))
+    def get_anime_info(self, link: str) -> Anime_info:
+        self.get_anime_page(link)
+        if self.root.xpath('//div[@class="player-blocked"]'):
+            raise AnimeGO_Exception('Anime is bloked')
+        xpath_query = '//script[@type="application/ld+json"]'
+        info: dict = json.loads(self.root.xpath(xpath_query)[0].text)
+
+        self.anime_info = Anime_info(
+            title=info.get('name'),
+            description=info.get('description'),
+            type=info.get('@type'),
+            numberOfEpisodes=len(self.root.xpath(
+                '//select[@name="series"]')[0].getchildren()),
+            poster=urljoin(self.domain, info.get('image')),
+            genre=info.get('genre')
         )
+        return self.anime_info
+
+    def get_dubs(self, link: str, episode_num: int):
+        header = {
+            'Host': 'animego.org',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'X-Requested-With': 'XMLHttpRequest',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': link,
+            'Cookie': 'device_view=full; censored=true',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin'
+        }
+        episode_id: str = self.root.xpath(
+            '//select[@name="series"]'
+        )[0].getchildren()[episode_num].get("value")
+
+        series: dict = requests.get(
+            "https://animego.org/anime/series",
+            params={'id': episode_id},
+            headers=header
+        ).json()
+
+        if series.get("status") == "success" and not series.get("blockEpisode"):
+            content: etree._Element = etree.fromstring(
+                series.get("content"),
+                etree.HTMLParser(encoding="utf-8"))
+            dub = content.xpath('//div[@id="video-dubbing"]/span')
+            vid = content.xpath('//div[@id="video-players"]')[0]
+            dubs = set(map(
+                lambda x: (
+                    x.xpath('span')[0].text.strip(),
+                    tuple(map(
+                        lambda x: x.get("data-player"),
+                        vid.xpath(
+                            f"""span[@data-provide-dubbing="{x.get('data-dubbing')}"]""")
+                    ))
+                ),
+                dub))
+            return tuple(map(lambda x: Dub(dubber=x[0], links=x[1]), dubs))
 
 
 if __name__ == "__main__":
     ag = AnimeGo_Parser()
-    anime = ag.search("Владыка")
-    ag.get_anime_info(anime[1].link)
+    anime_finds = ag.search("евангелион")
+    for anime_find in anime_finds:
+        print("###############")
+        print(anime_find.title, "-", anime_find.link)
+        print()
 
+        try:
+            anime_info = ag.get_anime_info(anime_find.link)
+            print(anime_info.title)
+            print(anime_info.type)
+            print(anime_info.numberOfEpisodes)
+            print()
+
+            dubs = ag.get_dubs(anime_finds[1].link, 0)
+            print(tuple(map(lambda x: x.dubber, dubs)))
+        except Exception as e:
+            if e == "Anime is bloked":
+                print("Anime is bloked")
